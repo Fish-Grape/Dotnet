077fe149003a3020675ddd4b2ea77560eacbcab6
FishLiuWei <502152178@qq.com>
2018-12-03 18:11:14 +0800

    仓储模式

---
+++ RepositoryTest/RepositoryTest/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RepositoryTest
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        List<TEntity> FetchAll();
+        IQueryable<TEntity> Query{get;}
+
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+        void Save();
+    }
+}
---
+++ RepositoryTest/RepositoryTest/TestTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace RepositoryTest
+{
+    [Table(Name = "TestTable")]
+    class TestTable 
+    {
+
+        public TestTable()
+        {
+
+        }
+
+        public TestTable(Guid TestTableGUID, string TestTableName, string TestTableAddress)
+        {
+            this.TestTableGUID = TestTableGUID;
+            this.TestTableName = TestTableName;
+            this.TestTableAddress = TestTableAddress;
+        }
+
+        [Column(IsPrimaryKey=true)]
+        public Guid ? TestTableGUID { set; get; }
+
+        [Column]
+        public string TestTableName { set; get; }
+        [Column]
+        public string TestTableAddress { set; get; }
+
+
+    }
+}
